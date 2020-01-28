ba3eea25649738558bad7ce4adffdc0029a852cf
unknown <tgarmita@gmail.com>
2020-01-28 14:26:48 -0700

    add score tracking

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,14 +1,14 @@
 import React from "react";
 const Fragment = React.Fragment;
 
-const Rank = ({ name, score }) => {
+const Rank = ({ name, totalScore }) => {
   return (
     <Fragment>
       <div className="f1 white">{`Hello ${name}! Welcome back.`}</div>
       <div
         style={{ color: "#FFD700" }}
         className="f2 white"
-      >{`Your score is: ${score}`}</div>
+      >{`Your score is: ${totalScore}`}</div>
     </Fragment>
   );
 };
