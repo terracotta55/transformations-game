071a4b3a18ff60758ef0bbbf628d084951fd52bf
Maria Paz <pazguada@gmail.com>
2020-03-13 14:37:58 -0600

    Show point labels

--- src/components/Game/TriangleShape.js
+++ src/components/Game/TriangleShape.js
@@ -23,11 +23,22 @@ class TriangleShape extends Component {
           fill={this.props.color}
           points={`${cax} ${cay}, ${cbx} ${cby}, ${ccx} ${ccy}`}
         ></polygon>
-
-        {/* point labels */}
-        {/* <text fontWeight="bold" x={cax} y={cay - (Math.sign(this.props.a.y) * 10)}>{`(${this.props.a.x}, ${this.props.a.y})`}</text> */}
-        {/* <text font-weight="bold" x={cbx} y={cby - (Math.sign(this.props.b.y) * 10)}>{`(${this.props.b.x}, ${this.props.b.y})`}</text>
-        <text font-weight="bold" x={ccx} y={ccy - (Math.sign(this.props.c.y) * 10)}>{`(${this.props.c.x}, ${this.props.c.y})`}</text> */}
+        {/* point labels
+        <text
+          fontWeight="bold"
+          x={cax}
+          y={cay - Math.sign(this.props.a.y) * 10}
+        >{`(${this.props.a.x}, ${this.props.a.y})`}</text>
+        <text
+          font-weight="bold"
+          x={cbx}
+          y={cby - Math.sign(this.props.b.y) * 10}
+        >{`(${this.props.b.x}, ${this.props.b.y})`}</text>
+        <text
+          font-weight="bold"
+          x={ccx}
+          y={ccy - Math.sign(this.props.c.y) * 10}
+        >{`(${this.props.c.x}, ${this.props.c.y})`}</text> */}
       </>
     );
   }
